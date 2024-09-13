ca0357a2d6901cd878f90435599e3fd8357350ff
Jochen Klar <mail@jochenklar.de>
2024-09-13 13:06:21 +0200

    Add BaseZenodoExportProvider

--- rdmo_zenodo/exports.py
+++ rdmo_zenodo/exports.py
@@ -12,7 +12,59 @@ from rdmo.services.providers import OauthProviderMixin
 logger = logging.getLogger(__name__)
 
 
-class ZenodoExportProvider(OauthProviderMixin, Export):
+class BaseZenodoExportProvider(OauthProviderMixin, Export):
+
+    @property
+    def client_id(self):
+        return settings.ZENODO_PROVIDER['client_id']
+
+    @property
+    def client_secret(self):
+        return settings.ZENODO_PROVIDER['client_secret']
+
+    @property
+    def zenodo_url(self):
+        return settings.ZENODO_PROVIDER.get('zenodo_url', 'https://sandbox.zenodo.org').strip('/')
+
+    @property
+    def authorize_url(self):
+        return f'{self.zenodo_url}/oauth/authorize'
+
+    @property
+    def token_url(self):
+        return f'{self.zenodo_url}/oauth/token'
+
+    @property
+    def deposit_url(self):
+        return f'{self.zenodo_url}/api/records'
+
+    @property
+    def redirect_path(self):
+        return reverse('oauth_callback', args=['zenodo'])
+
+    def get_authorize_params(self, request, state):
+        return {
+            'response_type': 'code',
+            'client_id': self.client_id,
+            'scope': 'deposit:write',
+            'redirect_uri': request.build_absolute_uri(self.redirect_path),
+            'state': state
+        }
+
+    def get_callback_data(self, request):
+        return {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'authorization_code',
+            'redirect_uri': request.build_absolute_uri(self.redirect_path),
+            'code': request.GET.get('code')
+        }
+
+    def get_error_message(self, response):
+        return response.json().get('errors')
+
+
+class ZenodoExportProvider(BaseZenodoExportProvider):
 
     rights_uri_options = {
         'dataset_license_types/71': 'cc-by-4.0',
@@ -67,34 +119,6 @@ class ZenodoExportProvider(OauthProviderMixin, Export):
                 'title': _('ZENODO error'),
                 'errors': [_('The URL of the new dataset could not be retrieved.')]
             }, status=200)
-
-    @property
-    def client_id(self):
-        return settings.ZENODO_PROVIDER['client_id']
-
-    @property
-    def client_secret(self):
-        return settings.ZENODO_PROVIDER['client_secret']
-
-    @property
-    def zenodo_url(self):
-        return settings.ZENODO_PROVIDER.get('zenodo_url', 'https://sandbox.zenodo.org').strip('/')
-
-    @property
-    def authorize_url(self):
-        return f'{self.zenodo_url}/oauth/authorize'
-
-    @property
-    def token_url(self):
-        return f'{self.zenodo_url}/oauth/token'
-
-    @property
-    def deposit_url(self):
-        return f'{self.zenodo_url}/api/records'
-
-    @property
-    def redirect_path(self):
-        return reverse('oauth_callback', args=['zenodo'])
 
     def get_post_url(self):
         return self.deposit_url
@@ -175,24 +199,3 @@ class ZenodoExportProvider(OauthProviderMixin, Export):
         return {
             'metadata': metadata
         }
-
-    def get_authorize_params(self, request, state):
-        return {
-            'response_type': 'code',
-            'client_id': self.client_id,
-            'scope': 'deposit:write',
-            'redirect_uri': request.build_absolute_uri(self.redirect_path),
-            'state': state
-        }
-
-    def get_callback_data(self, request):
-        return {
-            'client_id': self.client_id,
-            'client_secret': self.client_secret,
-            'grant_type': 'authorization_code',
-            'redirect_uri': request.build_absolute_uri(self.redirect_path),
-            'code': request.GET.get('code')
-        }
-
-    def get_error_message(self, response):
-        return response.json().get('errors')
