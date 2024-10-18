e799ec5cb4b788b8eb9bf23af5dbcb6ec9f30aa6
David Wallace <david.wallace@tu-darmstadt.de>
2024-10-18 09:00:28 +0200

    feat,fix: add RDMO_PLUGIN_KEY and methods for pdf from snapshot upload
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo_zenodo/exports/base.py
+++ rdmo_zenodo/exports/base.py
@@ -3,14 +3,27 @@ import logging
 from django.conf import settings
 from django.shortcuts import reverse
 from django.utils.translation import gettext_lazy as _
+from django.http import HttpResponse
+from django.template import TemplateSyntaxError
 
+from rdmo.core.utils import render_to_format
 from rdmo.projects.exports import Export
+from rdmo.projects.utils import get_value_path
 from rdmo.services.providers import OauthProviderMixin
+from rdmo.views.models import View
 
 logger = logging.getLogger(__name__)
 
+json_header = {
+    'Content-Type': 'application/json',
+    }
+binary_header= {
+    'Content-Type': 'application/octet-stream',
+}
 
 class BaseZenodoExportProvider(OauthProviderMixin, Export):
+
+    RDMO_PLUGIN_KEY = None
 
     @property
     def client_id(self):
@@ -38,7 +51,18 @@ class BaseZenodoExportProvider(OauthProviderMixin, Export):
 
     @property
     def redirect_path(self):
-        return reverse('oauth_callback', args=['zenodo'])
+        if self.RDMO_PLUGIN_KEY is None:
+            raise ValueError("the RDMO_PLUGIN_KEY should be set as a class attribute")
+        return reverse('oauth_callback', args=[self.RDMO_PLUGIN_KEY])
+
+    def record_file_url(self, record_id):
+        return f"{self.deposit_url}/records/{record_id}/draft/files"
+
+    def record_file_content_url(self, record_id, file_key):
+        return f"{self.record_file_url(record_id)}/{file_key}/content"
+
+    def record_file_commit_url(self, record_id, file_key):
+        return f"{self.record_file_url(record_id)}/{file_key}/commit"
 
     def get_authorize_params(self, request, state):
         return {
@@ -58,5 +82,54 @@ class BaseZenodoExportProvider(OauthProviderMixin, Export):
             'code': request.GET.get('code')
         }
 
-    def get_error_message(self, response):
-        return response.json().get('errors')
+    def render_project_views(self, project, snapshot, attachments_format, view=None):
+
+        if view is None:
+            view = View.objects.get(uri="https://rdmorganiser.github.io/terms/views/variable_check")
+
+        try:
+            rendered_view = view.render(project, snapshot)
+        except TemplateSyntaxError:
+            return HttpResponse()
+
+        return render_to_format(
+            None, attachments_format, project.title, 'projects/project_view_export.html', {
+                'format': attachments_format,
+                'title': project.title,
+                'view': view,
+                'rendered_view': rendered_view,
+                'resource_path': get_value_path(project, snapshot)
+            }
+        )
+
+    def render_snapshot_to_pdf(self, snapshot):
+        # get the pdf
+        return self.render_project_views(self.project, snapshot, "pdf")
+
+
+    def upload_file(self, record_id, binary=None):
+        """
+        takes the record_id of the draft record and the file contents as binary data.
+        API_URL should be the zenodo API URL, e.g. 
+        """
+        # POST data / PDF
+        files_url = self.record_file_url(record_id)
+        file_data = [{"key": "DMP.pdf"}]
+        data_initialization_response = self.post(self.request, files_url, json=file_data)
+
+        # upload the actual file content as binary stream
+        # extract files/content URL from the data upload response
+        breakpoint()
+        # files_content_url = data_initialization_response.json()["entries"][0]["links"]["content"]
+        files_content_url = self.record_file_content_url(record_id, file_data[0]['key'])
+        # self.deposit_url+f"records/{record_id}/draft/files/{file_data[0]['key']}/content"
+        ## multiple files as upload even possible???                     ^^^
+
+        # upload with binary data
+        data_content_response = self.put(self.request, files_content_url, data=binary) #headers=binary_header,
+
+        # commit the file upload
+        file_commit_url = self.deposit_url+f"records/{record_id}/draft/files/{file_data[0]['key']}/commit"
+
+        data_commit_response = self.post(self.request, file_commit_url)
+        return data_commit_response
