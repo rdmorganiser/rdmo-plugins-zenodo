f216d40e22b10f652dafaffd2c6b34df2f1cd3a2
David Wallace <david.wallace@tu-darmstadt.de>
2025-07-28 16:28:27 +0200

    add view and export format to snapshot form
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo_zenodo/exports/forms.py
+++ rdmo_zenodo/exports/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 
@@ -18,10 +19,22 @@ class ZenodoSnapshotForm(forms.Form):
         required=False,  # Allows empty selection
         widget=forms.RadioSelect
     )
+    view = forms.ChoiceField(
+        label=_("Select the view with which your project will be published"),
+        required=False,  # Allows empty selection
+        widget=forms.Select,
+    )
+    export_format = forms.ChoiceField(
+        label=_("Select the export format"),
+        required=False, widget=forms.Select,
+        choices=settings.EXPORT_FORMATS
+    )
 
     def __init__(self, *args, **kwargs):
         snapshot_choices = kwargs.pop('snapshot_choices', [])
+        view_choices = kwargs.pop("view_choices", [])
         super().__init__(*args, **kwargs)
         snapshot_choices = [(None, _("Create new snapshot")), *snapshot_choices]
         self.fields['snapshot'].choices = snapshot_choices
         self.fields['snapshot'].initial = None
+        self.fields['view'].choices = view_choices
